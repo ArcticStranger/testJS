b8c2fa0c6b6eb5ebc503a83bf212c3c3581c54d1
Тимофей Атласов <Arctic_Stranger@Noutbuk-Saryal-Timofej.local>
2025-05-18 23:08:23 +0900

    continue to learn

---
+++ 01-base/app.js
@@ -0,0 +1,65 @@
+let num = 42
+let firstName = 'Timofey'
+const isProgrammer = true
+
+/* Can Do
+let $ = 'test'
+let $num = 42
+let num$ = 42
+let _ = 49
+let _num = 12
+let num_ = 12
+let first_name = 'Elena'
+let myNum = 34
+let num42 = 10
+*/
+
+
+/* Restricted 
+let 42num = '11'
+let my-num = 1
+let const 
+*/
+
+// alert(firstName)
+
+// console.log(num + 10)
+// console.log(num - 10)
+// console.log(num / 10)
+// console.log(num * 10)
+
+// let num2 = num + 10
+// console.log(num, num2)
+// num = num2 - num
+// num2 = num2 + 1
+// console.log(num, num2)
+
+// let num3 = num + 10 * 2 / 5 - 1
+// console.log(num3)
+
+// const fullName = firstName + ' Atlasov'
+// const fullName = firstName + ' ' + 'Atlasov'
+// console.log(fullName)
+
+// ====== Data types
+
+const age = 29
+const name = 'Timofey'
+const isProg = false 
+let x // undef
+// null
+// console.log(typeof (1 / x)) 
+// console.log(typeof NaN)
+
+// ============ Priority
+const fullAge = 29
+const birthYear = 1993
+const currentYear = 2023
+// > < >= <=
+const isFullAge = currentYear - birthYear >= fullAge
+console.log(isFullAge)
+
+const num1 = 42
+const num2 = '42'
+
+console.log(num1 === num2)
--- app.js
+++
@@ -1,4 +0,0 @@
-let num = 42
-let firstName = 'Timofey'
-const isProgrammer = true
-
